fd768f94e8a6140a89d4ccd18db209e7e192dfa7
sigmatri <13516148@std.stei.itb.ac.id>
2019-02-17 23:33:02 +0700

    ubah tipe skala ke float

--- Task3/skala.c
+++ Task3/skala.c
@@ -7,19 +7,19 @@
 /*
     menghasil kan koordinat titik yang letaknya n kali dari letak awal
 */
-void skala(int *x, int *y, int n) {
+void skala(int *x, int *y, float n) {
 /*initial state:
     x dan y merupakan titik yang akan di skala ulang, dengan n kali dari ukuran awal
 */
-    *x = (*x * n);
-    *y = (*y * n);  
+    *x = round(*x * n);
+    *y = round(*y * n);  
     return;
 }    
 int main() {
     int x,y;
     x=3;
     y=3;
-    skala(&x,&y,2);
+    skala(&x,&y,0.5);
     printf("%d %d",x,y);
 
     return 0;
