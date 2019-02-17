128b1bcc97209cca3c52ba15fa3a68cbbaa3b0bb
sigmatri <13516148@std.stei.itb.ac.id>
2019-02-17 23:19:33 +0700

    tambah file fungsi skala

---
+++ Task3/skala.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+
+/*
+    menghasil kan koordinat titik yang letaknya n kali dari letak awal
+*/
+void skala(int *x, int *y, int n) {
+/*initial state:
+    x dan y merupakan titik yang akan di skala ulang, dengan n kali dari ukuran awal
+*/
+    *x = (*x * n);
+    *y = (*y * n);  
+    return;
+}    
+int main() {
+    int x,y;
+    x=3;
+    y=3;
+    rotasi (&x,&y,2);
+    printf("%d %d",x,y);
+
+    return 0;
+}
