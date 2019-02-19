79cfc8178d1e75b22bcdcf775b1b3fdbd4f24be6
hafisalrafi <13516034@std.stei.itb.ac.id>
2019-02-19 16:28:39 +0700

    update display map

--- Task5/denah.c
+++ Task5/denah.c
@@ -1,20 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "kamus.h"
 
 void testGetFile();
 
 int main() {
-    printf("titiknya adalah: \n");
-    testGetFile();
+    // Membuka driver framebuffer
+    fb_driver = open("/dev/fb0", O_RDWR);
+    if (fb_driver == -1) {
+        perror("Error: driver framebuffer tidak dapat dibuka");
+        exit(1);
+    }
 
+    // Get fixed screen information
+    if (ioctl(fb_driver, FBIOGET_FSCREENINFO, &fix_info) == -1) {
+        perror("Error reading fixed information");
+        exit(2);
+    }
+
+    // Get variable screen information
+    if (ioctl(fb_driver, FBIOGET_VSCREENINFO, &var_info) == -1) {
+        perror("Error reading variable information");
+        exit(3);
+    }
+
+    // Mapping framebuffer ke memori
+    int screensize = var_info.xres * var_info.yres * var_info.bits_per_pixel / 8;
+    fb = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_driver, 0);
+    if((int)fb == -1){
+        perror("Error: failed to map framebuffer device to memory");
+        exit(4);
+    }
+
+    clear_screen(800, 600);
+
+    color bg = {0,0,0,0};
+
+    testGetFile();
+    while(1) {
+
+    }
+
+    munmap(fb, screensize);
+    close(fb_driver);
     return 0;
 }
 
 
 void testGetFile() {
     // open file
-    FILE *fp = fopen("titik.txt", "r");
+    FILE *fp = fopen("MAP_ITB.txt", "r");
     size_t len = 255;
     // need malloc memory for line, if not, segmentation fault error will occurred.
     char *line = malloc(sizeof(char) * len);
@@ -27,20 +60,45 @@ void testGetFile() {
         //printf("%s", line);
         int jumlah = 0;
         int count = 0;
+        int x1,x2,x3,x4,y1,y2,y3,y4;
         for(int i=0; i<strlen(line); i++) {
             char kar = line[i+1];
             if ((kar == ',') || (kar == '-') || (kar == ' ')) {
                 jumlah += (int)line[i] - 48;
                 if (count == 0) {
-                    printf("(%d,", jumlah);
-                    count = 2;
-                } else {
-                    printf("%d)\n", jumlah);
+                    x1 = jumlah;
+                    count++;
+                } else if (count == 1) {
+                    y1 = jumlah;
+                    count++;
+                } else if (count == 2) {
+                    x2 = jumlah;
+                    count++;
+                } else if (count == 3) {
+                    y2 = jumlah;
+                    count++;
+                } else if (count == 4) {
+                    x3 = jumlah;
+                    count++;
+                } else if (count == 5) {
+                    y3 = jumlah;
+                    count++;
+                } else if (count == 6) {
+                    x4 = jumlah;
+                    count++;
+                } else if (count == 7) {
+                    y4 = jumlah;
                     count = 0;
                 }
                 jumlah = 0;
                 i++;
                 if (kar == '-') {
+                    color blue = {0,0,255,255};
+                    //printf ("%d,%d %d,%d %d,%d %d,%d\n",x1,y1,x2,y2,x3,y3,x4,y4);
+                    draw_garis(x1, x2, y1, y2, &blue);
+                    draw_garis(x2, x3, y2, y3, &blue);
+                    draw_garis(x3, x4, y3, y4, &blue);
+                    draw_garis(x4, x1, y4, y1, &blue);
                 	break;
                 }
             } else {
---
+++ Task5/kamus.h
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <math.h>
+#include <string.h>
+
+#define PI 3.14159265
+
+#define SCREEN_DIAGONAL 1000 // Jumlah maksimum titik yang ada pada diagonal layar
+
+typedef struct{
+    int r,g,b,a;
+} color;
+
+struct fb_var_screeninfo var_info;
+struct fb_fix_screeninfo fix_info;
+extern char *fb; //pointer framebuffer
+extern int fb_driver; //pointer framebuffer driver
+
+extern int layarx;
+extern int layary;
+
+unsigned char buffer_r[800][600];
+unsigned char buffer_g[800][600];
+unsigned char buffer_b[800][600];
+
+
+void draw_badan_left(int x, int y, color* c);
+void draw_kepala_left(int x, int y, color* c);
+void draw_pesawat_left(int x, int y, color* c);
+void draw_turret(color *c, int x);
+void draw_char_A(int x, int y);
+void draw_char_B(int x, int y);
+void draw_char_C(int x, int y);
+void draw_char_D(int x, int y);
+void draw_char_E(int x, int y);
+void draw_char_F(int x, int y);
+void draw_char_G(int x, int y);
+void draw_char_H(int x, int y);
+void draw_char_I(int x, int y);
+void draw_char_J(int x, int y);
+void draw_char_K(int x, int y);
+void draw_char_L(int x, int y);
+void draw_char_M(int x, int y);
+void draw_char_N(int x, int y);
+void draw_char_O(int x, int y);
+void draw_char_P(int x, int y);
+void draw_char_Q(int x, int y);
+void draw_char_R(int x, int y);
+void draw_char_S(int x, int y);
+void draw_char_T(int x, int y);
+void draw_char_U(int x, int y);
+void draw_char_V(int x, int y);
+void draw_char_W(int x, int y);
+void draw_char_X(int x, int y);
+void draw_char_Y(int x, int y);
+void draw_char_Z(int x, int y);
+void draw_char_1(int x, int y);
+void draw_char_2(int x, int y);
+void draw_char(int x, int y, char c);
+void draw_kata(int* x, int* y, char* kata);
+void draw_spasi(int x, int y);
+void draw_dot(int x, int y, color* c);
+void draw_garis(int x0, int x1, int y0, int y1, color* c);
+void clear_screen(int width, int height);
+void refresh(int x0, int x1, int y0, int y1);
+void jedug(int x0, int y0, int x1, int y1);
+void line(int x0, int y0, int x1, int y1, int buf[SCREEN_DIAGONAL][2], int *length);
+//function
+int isAnomali(int x, int y);
+
