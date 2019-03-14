280f7d324ed45cc3c2bc09a16ad8715e120586f1
Mohammad Husein Amrullah <mhuseinamr@gmail.com>
2019-03-14 22:19:59 +0700

    Bug fix: can't draw line with angle between 315 and 360 degrees.
    Actually changing the Bresenham algorithm to a naive float
    calculation hahaha.

--- Task6-UTS/kamus.c
+++ Task6-UTS/kamus.c
@@ -119,49 +119,27 @@ void draw_dot(int x, int y, color* c){
     kemudian memasukkannya ke dalam buf. Jumlah titik dimasukkan ke length.
 */
 void line(int x0, int y0, int x1, int y1, int buf[SCREEN_DIAGONAL][2], int *length) {
-    int dx, dy, sx, sy, err, e2, i;
+    int i, x, y;
+    double m, c;
 
-    dx = abs(x1-x0);
-    dy = abs(y1-y0);
+    /* Menghitung gradien dan konstanta */
+    m = (float) (y1 - y0) / (x1 - x0);
+    c = (float) y0 - (m * x0);
 
-    if (x0 < x1) {
-        sx = 1;
-    } else {
-        sx = -1;
-    }
-
-    if (y0 < y1) {
-        sy = 1;
-    } else {
-        sy = -1;
-    }
-
-    if (dx > dy) {
-        err = dx;
-    } else {
-        err = -dy/2;
-    }
-
+    x = x0;
+    y = y0;
     for (i = 0; i < SCREEN_DIAGONAL; i++) {
-        /* Mengisikan (x0, y0) ke elemen array ke-i */
-        buf[i][0] = x0;
-        buf[i][1] = y0;
+        /* Mengisikan (x, y) ke elemen array ke-i */
+        buf[i][0] = x;
+        buf[i][1] = y;
 
-        if (x0 == x1 && y0 == y1) {
-            *length = i;
+        if (x == x1 && y == y1) {
+            *length = i + 1;
             break;
         }
 
-        e2 = err;
-
-        if (e2 > -dx) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dy) {
-            err += dx;
-            y0 += sy;
-        }
+        x++;
+        y = round((m * x) + c);
     }
 }
 
